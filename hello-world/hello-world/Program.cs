9e352ab8ebb759fc0fae32cda676424039878c45
Henri Kangasmäki <henri.kangasmaki@gmail.com>
2018-09-05 13:56:21 +0300

    hello-world project

---
+++ hello-world/hello-world/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace hello_world
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Henri Kangasmäki");
+        }
+    }
+}
